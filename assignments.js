eaad93b95c8ac8915ecf4e935af073551cc46222
Nesgc <nestorgarciac11@gmail.com>
2024-02-26 22:41:04 -0600

    guess my number

--- assignments.js
+++ assignments.js
@@ -9,6 +9,7 @@ const calcTip = function (bill) {
     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
    
   }
+
   const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
   const tips = [];
   const totals = [];
@@ -19,7 +20,15 @@ const calcTip = function (bill) {
       totals.push(bills[i] + tip);
   }
   
-  console.log(bills);
-  console.log(tips);
-  console.log(totals);
+  console.log(bills, tips, totals);
   
+  const calcAverage = function(arr){
+    let sum = 0;
+    for(let i = 0 ; i<arr.length; i++){
+        sum=sum+arr[i];
+         
+    }
+    return sum/arr.length
+  }
+  console.log(calcAverage([2,3,7]
+  ))
